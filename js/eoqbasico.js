7b9f0f562b00358d23f046a59a9c54ffb999422a
Mateo Zubieta <mateozubieta13@gmail.com>
2022-06-08 09:22:47 -0400

    commit para subir la logica del eoq basico

---
+++ js/eoqbasico.js
@@ -0,0 +1,45 @@
+var y = document.getElementById("optimo");
+var tiem = document.getElementById("tiempo");
+var rops = document.getElementById("rop");
+var b = document.getElementById("resultado");
+b.addEventListener("click", mostrarDatos);
+
+
+function optimo (){
+    var demanda = parseFloat(document.getElementById("demanda").value);
+    var k = parseFloat(document.getElementById("k").value);
+    var h = parseFloat(document.getElementById("h").value);
+    var min = Math.sqrt((2*k*demanda)/h);
+    min = min.toFixed(2);
+    return min
+}
+
+function tiempo (){
+    var demanda = parseFloat(document.getElementById("demanda").value);
+    var opt = optimo();
+    var time = (opt/demanda);
+    time = time.toFixed(2);
+    return time
+}
+function rop(){
+    var demanda = parseFloat(document.getElementById("demanda").value);
+    var l = parseFloat(document.getElementById("l").value);
+    var time = tiempo();
+    var n = 0;
+    var rp = 0;
+    if (l < time){
+        rp = l*demanda;
+    }else{
+        n = Math.floor(l/t);
+        rp = l-(n*demanda);
+    }
+    console.log(n, rp, "gaaa");
+    rp = rp.toFixed(2);
+    return rp
+}
+
+function mostrarDatos() {
+    y.innerHTML = "y = "+optimo();
+    tiem.innerHTML="tiempo optimo= "+tiempo();
+    rops.innerHTML = " rop = "+rop();
+}
