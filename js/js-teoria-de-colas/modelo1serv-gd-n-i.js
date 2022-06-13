885878ccb05c71fabd10a24ba164766a585ce931
Mateo Zubieta <mateozubieta13@gmail.com>
2022-06-12 23:08:12 -0400

    commi para subir el modelo de colas mm1-gd/n/inf

--- js/js-teoria-de-colas/modelo1serv-gd-n-i.js
+++ js/js-teoria-de-colas/modelo1serv-gd-n-i.js
@@ -1,6 +1,7 @@
 var p=document.getElementById("p");
 var p0=document.getElementById("p0");
 var pn=document.getElementById("pn");
+var lambdaefec=document.getElementById("lamefec");
 var ls=document.getElementById("ls");
 var ws=document.getElementById("ws");
 var wq=document.getElementById("wq");
@@ -13,10 +14,11 @@ function mostrarDatos() {
     p.innerHTML="p= "+pchica();
     p0.innerHTML="p0= "+p_0();
     pn.innerHTML="pn= "+p_n();
+    lambdaefec.innerHTML="lamda efec= "+lambda_efec();
     ls.innerHTML="Ls= "+l_s();
     ws.innerHTML="Ws= "+w_s();
     wq.innerHTML="Wq= "+w_q();
-    lq.innerHTML="Ls= "+l_q();
+    lq.innerHTML="Lq= "+l_q();
     c.innerHTML="c= "+c_guion();
 }
 
@@ -29,47 +31,64 @@ function pchica() {
 }
 
 function p_0(){
-    var p0=1-pchica();
+    var n=parseFloat(document.getElementById("nmayus").value);
+    var p0=(1-pchica())/(1-Math.pow(pchica(),n+1));
     p0=p0.toFixed(5);
     return p0
 }
 
 function p_n(){
     var n=parseFloat(document.getElementById("n").value);
-    var pn=p_0()*Math.pow(pchica(),n);
+    var nmayus=parseFloat(document.getElementById("nmayus").value);
+    var pn=((1-pchica())*Math.pow(pchica(),n))/(1-Math.pow(pchica(),nmayus+1));
     pn=pn.toFixed(5);
     return pn
 }
 
+function lambda_efec(){
+    var lambda=parseFloat(document.getElementById("lambda").value);
+    var n=parseFloat(document.getElementById("nmayus").value);
+    var pn=((1-pchica())*Math.pow(pchica(),n))/(1-Math.pow(pchica(),n+1));
+    var lambdaefec=lambda*(1-pn);
+    lambdaefec=lambdaefec.toFixed(5);
+    return lambdaefec
+}
+
 function l_s(){
-    var ls=pchica()/p_0();
-    ls=ls.toFixed(5);
-    return ls
+    var n=parseFloat(document.getElementById("nmayus").value);
+    if(pchica==1){
+        var ls=n/2;
+        ls=ls.toFixed(5);
+        return ls
+    }else{
+        var ls=(pchica()*(1-((n+1)*Math.pow(pchica(),n))+(n*Math.pow(pchica(),n+1))))/((1-pchica())*(1-Math.pow(pchica(),n+1)));
+        ls=ls.toFixed(5);
+        return ls
+    }
 }
 
 function w_s(){
-    var lambda=parseFloat(document.getElementById("lambda").value);
-    var niu=parseFloat(document.getElementById("niu").value);
-    var ws=1/(niu-lambda);
+    var ws=l_s()/lambda_efec();
     ws=ws.toFixed(5);
     return ws
 }
 
 function w_q(){
     var niu=parseFloat(document.getElementById("niu").value);
-    var wq=pchica()/(niu*p_0());
+    wq=w_s()-(1/niu);
     wq=wq.toFixed(5);
     return wq
 }
 
 function l_q(){
-    var lq=Math.pow(pchica(),2)/p_0();
+    var niu=parseFloat(document.getElementById("niu").value);
+    lq=-((lambda_efec()-(l_s()*niu))/niu);
     lq=lq.toFixed(5);
     return lq
 }
 
 function c_guion(){
-    var c=l_s()-l_q();
+    c=l_s()-l_q();
     c=c.toFixed(5);
     return c
 }
